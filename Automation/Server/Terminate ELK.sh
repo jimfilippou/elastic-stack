47cabe90cc8c6084a9f483ae17f146844f2adec4
Jim <jimfilippou@hotmail.gr>
2016-11-18 16:46:57 +0200

    Create Terminate ELK.sh

---
+++ Automation/Server/Terminate ELK.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo Killing Elastic Search...
+/etc/init.d/elasticsearch stop
+echo Killing Logstash...
+systemctl stop logstash.service
+echo Killing Filebeat...
+/etc/init.d/filebeat stop
+echo Killing Kibana...
+pkill Kibana
