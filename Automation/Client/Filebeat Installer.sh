92c5e7a8d08c0d4c72bcf9c469086220d2a37a6d
FrozenVortex <jimfilippou@hotmail.gr>
2016-11-19 01:33:36 +0200

    Fixed Output

--- Automation/Client/Filebeat Installer.sh
+++ Automation/Client/Filebeat Installer.sh
@@ -2,11 +2,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-CYAN='\033[0;36m'
-RED='\033[0;31m'
-NC='\033[0m'
-echo [*] ${CYAN}Downloading${NC} Filebeat...
+echo [*] Downloading Filebeat...
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-amd64.deb
-echo [*] ${CYAN}Installing${NC} Filebeat...
+echo [*] Installing Filebeat...
 dpkg -i filebeat-5.0.0-amd64.deb
-echo Filebeat Installed. Make sure you ${RED}UPDATE${NC} the filebeat.yml ${RED}CONFIGURATION${NC}
+echo Filebeat Installed. Make sure you UPDATE the filebeat.yml CONFIGURATION
+ 
