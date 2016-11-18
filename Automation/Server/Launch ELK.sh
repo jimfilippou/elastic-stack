8e6b7f08d9efbc90ac6d34f88e1ee7ccece2e26d
Jim <jimfilippou@hotmail.gr>
2016-11-18 16:45:44 +0200

    Create Launch ELK.sh

---
+++ Automation/Server/Launch ELK.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo Starting Elastic Search...
+/etc/init.d/elasticsearch start
+sleep 10
+echo Starting Logstash...
+systemctl start logstash.service
+sleep 10
+echo Starting Filebeat...
+/etc/init.d/filebeat start
+sleep 10
+echo Starting Kibana...
+./kibana-5.0.0-linux-x86_64/bin/kibana
