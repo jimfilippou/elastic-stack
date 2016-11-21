56068bfa3c9e904f84f11d42c69a3c021d18134e
FrozenVortex <jimfilippou@hotmail.gr>
2016-11-21 18:54:41 +0200

    X-Pack Fixes

---
+++ X-Pack/license_import.sh
@@ -0,0 +1,9 @@
+
+if [ ! -f license.json ]; then
+    echo "License not found!"
+    exit 1
+fi
+echo Importing License...
+curl -XPUT -u elastic 'http://127.0.0.1:9200/_xpack/license' -d @license.json
+echo Importing License acknowledge=true ...
+curl -XPUT -u elastic 'http://127.0.0.1:9200/_xpack/license?acknowledge=true' -d @license.json
