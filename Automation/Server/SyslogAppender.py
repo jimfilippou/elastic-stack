334161729c9902dba177ae872df5c5fcd988aeff
FrozenVortex <jimfilippou@hotmail.gr>
2016-11-23 14:52:35 +0200

    Added SyslogAppender

---
+++ Automation/Server/SyslogAppender.py
@@ -0,0 +1,13 @@
+import os
+
+path = "/var/log/"
+file = "syslog"
+
+while True:
+    x = raw_input("Log => ")
+    if x == "exit":
+        print("Exited")
+        break
+    else:
+        os.system("echo '" + str(x) + "'' >> " + path + file)
+    
